612d1bcc5d539ff2371b3e20251db1ab089e6269
DavidTheGod420 <davidsand421@gmail.com>
2026-03-03 23:12:50 -0500

    Change name to David and add reflection comments
    Updated name in comments and added reflection section.

--- person.cpp
+++ person.cpp
@@ -1,5 +1,5 @@
 /*
-Name: John Doe
+Name: david 
 Course: CSCI 272
 Lesson Number: Lesson 4
 Code Topic: Copy Constructor Example
@@ -25,3 +25,16 @@ int main(){
 Person person1;
 person1.introduce();
 }
+/*
+Reflection:
+1. What I understood:
+ I understood how the copy constructor creates a new object and file while using commands in previous file
+2. Difficulties:
+ I initially struggled with understanding how 2 files can be dependent on eachother while using a constructor
+3. Research:
+ I searched online about different constructors and their purposes
+4. AI Usage:
+ used google ai search to narrow down a simple definition.
+5. What I learned:
+ learned how contructors allow 2 files to work with each other.
+*/
